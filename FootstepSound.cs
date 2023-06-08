044b82393f0d387dcb935f2d3983f50a1ff2689b
Joel Elrod <joelelrod246@gmail.com>
2023-06-08 18:03:03 -0400

    Initial Commit
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

---
+++ FootstepSound.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepSound : MonoBehaviour
+// Plays footsteps at the end of each headbob animation.
+{
+    void Start()
+    {
+        Debug.Log("test");
+    }
+}
